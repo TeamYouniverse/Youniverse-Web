d242db24727d6760b7bfe62c4cc96f28bee3d240
nahee-park <jessica396@ewhain.net>
2021-05-26 12:12:24 +0900

    chore: Api주소 수정

--- src/lib/getApi.js
+++ src/lib/getApi.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 // post를 통해 받은 id값을 넘겨줌
 const getApi = async (paramId) => {
+  const url =
+    'http://ec2-3-37-72-139.ap-northeast-2.compute.amazonaws.com:5000/api/login';
   //axios.get을 이용해 url에 존제하는 자원을 요청
   try {
-    const { data } = await axios.get(
-      'http://ec2-15-164-219-99.ap-northeast-2.compute.amazonaws.com:5000/api/login' +
-        paramId,
-    );
+    const data = await axios.get(`${url}` + paramId);
     console.log('[SUCCESS] GET user data', data);
     return data;
   } catch (e) {
--- src/lib/postApi.js
+++ src/lib/postApi.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 //userName을 받으면 poastApi(userName);을 실행시켜서 post를 시킴
 const url =
-  'http://ec2-15-164-219-99.ap-northeast-2.compute.amazonaws.com:5000/api/login';
+  'http://ec2-3-37-72-139.ap-northeast-2.compute.amazonaws.com:5000/api/login/';
 
 const postAPI = async (userName) => {
   try {
-    const { data } = await axios.post(`${url}`, {
+    const data = await axios.post(`${url}`, {
       username: `${userName}`,
     });
     console.log(data);
-    return data.id;
+    return data;
   } catch (e) {
     console.error('[FAIL] POST ANSWER', e);
     return e;
